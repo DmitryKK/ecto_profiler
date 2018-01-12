0ff3675e87f7df3f6a27c607588f3ed86aba17ac
DmitryKK <dimak33@ya.ru>
2018-01-12 22:57:45 +0300

    init

---
+++ lib/ecto_profiler.ex
@@ -0,0 +1,18 @@
+defmodule EctoProfiler do
+  @moduledoc """
+  Documentation for EctoProfiler.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoProfiler.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_profiler_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoProfilerTest do
+  use ExUnit.Case
+  doctest EctoProfiler
+
+  test "greets the world" do
+    assert EctoProfiler.hello() == :world
+  end
+end
